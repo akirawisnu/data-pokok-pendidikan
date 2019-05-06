35afd49aac423f8e3405e65d09376fe6b62f81d7
Lukman Edwindra <lukman.edwindra@taralite.com>
2019-05-06 11:44:29 +0700

    fixed .py script

--- src/data_pokok_pendidikan.py
+++ src/data_pokok_pendidikan.py
@@ -1,9 +1,11 @@
-import json
 import pandas as pd
 import requests
+from random import randint
+from time import sleep
 
 def get_province():
     """
+    Returns summary data at province level.
     """
 
     df = 'http://dapo.dikdasmen.kemdikbud.go.id/rekap/progres?'
@@ -14,61 +16,82 @@ def get_province():
     
     return df
 
-def get_district():
+def get_district(randmin=None, randmax=None):
     """
+    Returns summary data at district level.
     """
     
     df_prov = get_province()
+    prov_code = df_prov['kode_wilayah'].unique()
     district_by_prov = [None] * len(df_prov)
     df = pd.DataFrame()
-    for i in range(len(df_prov)):
-        dapodik_url = 'http://dapo.dikdasmen.kemdikbud.go.id/rekap/progres?'
-        district_url = 'id_level_wilayah=1&kode_wilayah={}&semester_id=20182'.format(df_prov['kode_wilayah'][i])
-        district_by_prov[i] = dapodik_url + district_url
-        district_by_prov[i] = requests.get(district_by_prov[i])
-        district_by_prov[i] = district_by_prov[i].json()
-        district_by_prov[i] = pd.DataFrame(district_by_prov[i])
-        df = df.append(district_by_prov[i])
-        df['kode_wilayah'] =  df['kode_wilayah'].apply(lambda x: x.replace(" ", ""))    
+
+    for i in range(len(prov_code)):
+        try:
+            dapodik_url = 'http://dapo.dikdasmen.kemdikbud.go.id/rekap/progres?'
+            district_url = 'id_level_wilayah=1&kode_wilayah={}&semester_id=20182'.format(prov_code[i])
+            district_by_prov[i] = dapodik_url + district_url
+            district_by_prov[i] = requests.get(district_by_prov[i])
+            district_by_prov[i] = district_by_prov[i].json()
+            district_by_prov[i] = pd.DataFrame(district_by_prov[i])
+            df = df.append(district_by_prov[i])
+            df['kode_wilayah'] =  df['kode_wilayah'].apply(lambda x: x.replace(" ", ""))
+            sleep(randint(randmin, randmax))
+        except requests.exceptions.HTTPError:
+            print("HTTP error")
+            pass
         
     return df
 
-def get_subdistrict():
+def get_subdistrict(randmin=None, randmax=None):
     """
+    Returns summary data at subdistrict level.
     """
         
     df_district = get_district()
-    subdistrict_by_district = [None] * len(df_district)
+    district_code = df_district['kode_wilayah'].unique()
+    subdistrict_by_district = [None] * len(district_code)
     df = pd.DataFrame()
-    for i in range(len(df_district)):
+
+    for i in range(len(district_code)):
         try:
             dapodik_url = 'http://dapo.dikdasmen.kemdikbud.go.id/rekap/progres?'
-            subdistrict_url = 'id_level_wilayah=2&kode_wilayah={}&semester_id=20182'.format(df_district['kode_wilayah'][i])
+            subdistrict_url = 'id_level_wilayah=2&kode_wilayah={}&semester_id=20182'.format(district_code[i])
             subdistrict_by_district[i] = dapodik_url + subdistrict_url
             subdistrict_by_district[i] = requests.get(subdistrict_by_district[i])
             subdistrict_by_district[i] = subdistrict_by_district[i].json()
             subdistrict_by_district[i] = pd.DataFrame(subdistrict_by_district[i])
             df = df.append(subdistrict_by_district[i])
-            df['kode_wilayah'] =  df['kode_wilayah'].apply(lambda x: x.replace(" ", "")) 
-        except:
+            df['kode_wilayah'] =  df['kode_wilayah'].apply(lambda x: x.replace(" ", ""))
+            sleep(randint(randmin, randmax))
+        except requests.exceptions.HTTPError:
+            print("HTTP error")
             pass
         
     return df
 
-def get_school():
+def get_school(randmin=None, randmax=None):
     """
+    Returns summary data at school level.
     """
-        
+
     df_subdistrict = get_subdistrict()
+    subdistrict_code = df_subdistrict['kode_wilayah'].unique()    
     school_by_subdistrict = [None] * len(df_subdistrict)
     df = pd.DataFrame()
-    for i in range(len(df_subdistrict)):
-        school_by_subdistrict[i] = 'http://dapo.dikdasmen.kemdikbud.go.id/rekap/progresSP?id_level_wilayah=3&kode_wilayah={}&semester_id=20182'.format(df_subdistrict['kode_wilayah'][i])
-        school_by_subdistrict[i] = requests.get(school_by_subdistrict[i])
-        school_by_subdistrict[i] = school_by_subdistrict[i].json()
-        school_by_subdistrict[i] = pd.DataFrame(school_by_subdistrict[i])
-        df = df.append(school_by_subdistrict[i])
-        
+
+    for i in range(len(subdistrict_code)):
+        try:
+            school_by_subdistrict[i] = 'http://dapo.dikdasmen.kemdikbud.go.id/rekap/progresSP?id_level_wilayah=3&kode_wilayah={}&semester_id=20182'.format(subdistrict_code[i])
+            school_by_subdistrict[i] = requests.get(school_by_subdistrict[i])
+            school_by_subdistrict[i] = school_by_subdistrict[i].json()
+            school_by_subdistrict[i] = pd.DataFrame(school_by_subdistrict[i])
+            df = df.append(school_by_subdistrict[i])
+            sleep(randint(randmin, randmax))
+        except requests.exceptions.HTTPError:
+            print("HTTP error")
+            pass            
+
     return df
 
 def get_school_detail():
